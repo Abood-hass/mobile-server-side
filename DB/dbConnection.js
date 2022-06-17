23ffe76c66905c50de5a11450a7a6a0fb63c893f
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2022-06-17 19:44:57 +0300

    Update dbConnection.js

--- DB/dbConnection.js
+++ DB/dbConnection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 require('dotenv').config()
 exports.con = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_SCHEMA
+    host: process.env.DB_HOST || "us-cdbr-east-05.cleardb.net",
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USERNAME || "bf866bc64138a1",
+    password: process.env.DB_PASSWORD || "06f5406c",
+    database: process.env.DB_SCHEMA || "heroku_37bb97e0f5ae5b0"
   });
 
 
