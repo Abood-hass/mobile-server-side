751ead282c3e83ee55bba907e755c1451621e736
Abood-hass <60661627+Abood-hass@users.noreply.github.com>
2022-06-18 11:46:22 +0300

    Register & Login Finishing

--- Controller/accountController.js
+++ Controller/accountController.js
@@ -2,49 +2,67 @@ const { default: ShortUniqueId } = require("short-unique-id");
 const uid = new ShortUniqueId({ length: 10 });
 const { con } = require("../DB/dbConnection");
 const generateAccessToken = require("./tokenController");
-// const { v4 : uuidv4 } = require("uuid");
-
 exports.ReigistNewAccount = (FirstName, LastName, Email, PhoneNumber1, PhoneNumber2, Password,
-     Gender, Governorate, Neighborhood, HouseNumber, NavigationalMark, DateOfBirth) => {
+  Gender, Governorate, Neighborhood, HouseNumber, NavigationalMark, DateOfBirth) => {
 
-      var CustomerID = uid();
-      
-    var token = generateAccessToken({ username: CustomerID });
-    var result;
-    try {   
-        var query = 
-        "INSERT INTO `heroku_37bb97e0f5ae5b0`.`customeraccount` (`CustomerID`, `FirstName`, `LastName`, `Email`, `PhoneNumber1`, `PhoneNumber2`, `Password`, `Gender`, `Governorate`, `Neighborhood`, `HouseNumber`, `NavigationalMark`, `DateOfBirth`, `token`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  var CustomerID = uid();
 
-        con.query(
-        query,[CustomerID, FirstName, LastName, Email, PhoneNumber1, PhoneNumber2, Password, 
-            Gender, Governorate, Neighborhood, HouseNumber, NavigationalMark, DateOfBirth, token],
-            (err, result, rows) => {
-              if (err) result = err;
-          
-              result = `Added ${rows} row(s)`;
-            });
-    } catch (err) {
-      result = err;
-    }
-      return result;
+  var token = generateAccessToken({ username: CustomerID });
+  var result = null;
+  try {
+    var query =
+      "INSERT INTO `heroku_37bb97e0f5ae5b0`.`customeraccount` (`CustomerID`, `FirstName`, `LastName`, `Email`, `PhoneNumber1`, `PhoneNumber2`, `Password`, `Gender`, `Governorate`, `Neighborhood`, `HouseNumber`, `NavigationalMark`, `DateOfBirth`, `token`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    con.query(
+      query, [CustomerID, FirstName, LastName, Email, PhoneNumber1, PhoneNumber2, Password,
+      Gender, Governorate, Neighborhood, HouseNumber, NavigationalMark, DateOfBirth, token],
+      (err, result, rows) => {
+        // if (err) result = err;
+        if (err != null) console.log(err.message);
+
+      });
+  } catch (err) {
+    result = err;
+  }
+  return result;
 }
 
-exports.LoginExsistAccount =  async (Email,Password) => {
-  try {  
-     
-    var query = 
-    "SELECT * FROM heroku_37bb97e0f5ae5b0.customeraccount WHERE email=? AND password=? and BanStatus='Unbanned';";
 
-    var  Result; 
-    con.query(query, [Email,Password], function (err, result) {
-      if (err) throw err;
-      // Result = result;
+function LoginExsistAccount(err, results) {
+
+  var LoginToken;
+
+  if (results[0] === undefined) { console.log("no account found"); }
+  else {
+    console.log("rows'", results, "'");
+    var CustomerID = results[0].CustomerID;
+    LoginToken = generateAccessToken({ username: CustomerID });
+    query =
+      "UPDATE `heroku_37bb97e0f5ae5b0`.`customeraccount` SET `token` = ? WHERE (`CustomerID` = ?); ";
+    con.query(query, [LoginToken, CustomerID])
+  }
+
+  if (err) throw err;
+  return (LoginToken);
+}
+
+exports.LoginExsistAccount = async (Email, Password) => {
+  let Result = "";
+  // global.Result= "";
+  try {
+    var query =
+      "SELECT * FROM heroku_37bb97e0f5ae5b0.customeraccount WHERE email=? AND password=? and BanStatus='Unbanned'";
+
+    con.query(query, [Email, Password], (err, results) => {
+      var Result = LoginExsistAccount(err, results)
       console.log(Result);
-      return Result;
-    });
-} catch (err) {
-  Result = err;
-}
-return Result;
+
+    }
+    );
+  } catch (err) {
+    Result = err;
+  }
+      // console.log("2", global.Result);
+  return Result;
 
 }
--- Routes/login.js
+++ Routes/login.js
@@ -1,19 +1,69 @@
 const { router } = require('../app');
+const { check, validationResult } = require('express-validator');
 const { ReigistNewAccount, LoginExsistAccount } = require('../Controller/accountController');
 
-router.post('/reigister', async (req, res) => {
+
+const UserRegistration = [
+    check('FirstName', 'First Name length from 3 to 10')
+        .isLength({ min: 3, max: 10 }),
+    check('LastName', ' Last Name length from 3 to 10')
+        .isLength({ min: 3, max: 10 }),
+    check('Email', 'Email number should be real Email')
+        .isLength({ min: 20, max: 30 }),
+    check('Password', 'Password length should be 8 to 15 characters')
+        .isLength({ min: 8, max: 15 }),
+    check('PhoneNumber1', 'Phone Number length should be 10 and numbers are allowed only')
+        .isLength({ min: 10, max: 10 }).isNumeric(),
+    check('PhoneNumber2')
+        .custom((value) => {
+            if (value.length !== 0 && value.length !== 10) {
+                return Promise.reject('PhoneNumber2 length should be 10');
+            } else {
+                return true;
+            }
+        }),
+    check('Governorate', 'Insert Governorate Correct')
+        .isLength({ min: 3 }),
+    check('Neighborhood', 'Insert Neighborhood Correct')
+        .isLength({ min: 3 }),
+
+]
+
+const UserLogin = [
+    check('Email', 'Email number should be real Email')
+        .isLength({ min: 20, max: 30 }),
+    check('Password', 'Password length should be 8 to 15 characters')
+        .isLength({ min: 8, max: 15 }),
+]
+
+
+
+
+router.post('/reigister', 
+[UserRegistration],
+ async (req, res) => {
     var data = req.body
-    
-    var result = ReigistNewAccount( data.FirstName, data.LastName, data.Email, data.PhoneNumber1, data.PhoneNumber2, data.Password,
-    data.Gender,data.Governorate, data.Neighborhood, data.HouseNumber, data.NavigationalMark, data.DateOfBirth)
+
+    var result = ReigistNewAccount(data.FirstName, data.LastName, data.Email, data.PhoneNumber1, data.PhoneNumber2, data.Password,
+        data.Gender, data.Governorate, data.Neighborhood, data.HouseNumber, data.NavigationalMark, data.DateOfBirth)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     res.send(result)
 })
 
-router.get('/login', async (req, res) => {
+router.get('/login', [UserLogin], async (req, res) => {
     var data = req.body
-    var result = LoginExsistAccount(data.Email,data.Password);
-    console.log(result);
-    res.send(result);
+    var result = await LoginExsistAccount(data.Email, data.Password);
+    res.setHeader('JWT-Token-Hash', result);
+    res.send(result)
+
 })
+
+router.get('/me', function(req, res) {
+    console.log("hi from 'console'");
+    res.send("hi from 'respone'");
+  });
 
 module.exports = router;
